262bebc75b7c35462592998011a68efcc6b19bbb
katerina.vg <greekdutchkat@gmail.com>
2023-11-17 21:38:39 -0800

    implement register and login, logout functionality

--- api/index.js
+++ api/index.js
@@ -7,15 +7,79 @@ also do npm install cors
 
 const express = require('express');
 const cors = require('cors');
-const app= express();
+const app = express();
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const bcrypt = require('bcryptjs'); //for hashing/encrypting passwords
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser'); 
+const { response } = require('express');
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10); //for hashing/encrypting passowrds
+const secret = 'anphhgrue795927ncuepq57839badjjiown'; //for webtokens
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+//if youre using credentials you need to specify more info in cors
 app.use(express.json());
+//add cookie parser npm install cookie-parser
+app.use(cookieParser());
 
-app.post('/register', (request,response) => { 
+//need to connect to mongo database
+mongoose.connect('mongodb+srv://beehive-blog:zcrbMRKyWz6fPzuq@cluster0.7dazpkm.mongodb.net/?retryWrites=true&w=majority');
+
+
+app.post('/register', async (request,response) => { 
     const {username, password} = request.body;
-    response.json({requestData:{username,password}});
+    try{
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password,salt),
+        });
+        response.json(userDoc);
+    }
+        catch(e) {
+            console.log(e);
+            response.status(400).json(e);
+        }
 });
 
+app.post('/login', async (request,response) => {
+    const {username,password} = request.body;
+    //grab the user
+    const userDoc = await User.findOne({username}); //same as username:username
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    // response.json(passOk);
+    if (passOk) {
+        //user is logged in
+        //this is where I install jsonwebtoken
+        //send a token
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            response.cookie('token', token).json('ok');
+        });
+    }
+    else {
+        //not logged in
+        response.status(400).json('wrong credentials');
+    }
+}); 
+
+//check if logged in by token inside cookies, and check if the token is valid
+app.get('/profile', (request,response) => {
+    const {token} = request.cookies; //grab the token
+    jwt.verify(token, secret, {}, (err,info) => {
+        if (err) throw err;
+        response.json(info);
+    });
+    // response.json(request.cookies);
+});
+
+app.post('/logout', (request,response) => {
+    response.cookie('token', '').json('ok');
+})
+
 app.listen(4000); //on port 4000
+//npm install mongodb in api
+//mongodb+srv://beehive-blog:zcrbMRKyWz6fPzuq@cluster0.7dazpkm.mongodb.net/?retryWrites=true&w=majority
+//beehive-blog pass: zcrbMRKyWz6fPzuq
 
--- client/beehive/src/Header.js
+++ client/beehive/src/Header.js
@@ -1,12 +1,44 @@
-import {Link} from "react-router-dom"
+import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
 
-export default function Header(){
+export default function Header() {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      // method: 'GET', //added, dont know if bad
+      credentials: 'include',
+    }).then(Response => {
+      Response.json().then(userInfo => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout() {
+    //want to invalidate the cookie
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUsername(null);
+  }
+
     return(
     <header>
     <Link to="/" className="logo">BeeHive</Link>
     <nav>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      {username && (
+        <>
+          <Link to="/create">Create new post</Link>
+          <a onClick={logout}>Logout</a>
+        </>
+      )}
+      {!username && (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
     </nav>
   </header>
  );
